c4d087ab501b68df2fd25a5f203df709f97545d9
warramma <wrammazy@mail.bradley.edu>
2026-04-01 10:13:56 -0500

    update gitignore, add php boilerplate

--- db_test.php
+++ db_test.php
@@ -1,15 +1,63 @@
 <?php
-$servername = "localhost";
-$username = "your_schema_name"; // Use the user you created [cite: 112]
-$password = "your_password";
-$dbname = "your_schema_name";   // Use the schema name you chose [cite: 111]
+error_reporting(0);
+mysqli_report(MYSQLI_REPORT_OFF);
+$import_attempted = false;
+$import_succeeded = false;
+$import_error_message = "";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $import_attempted = true;
+    $con = mysqli_connect("localhost","pizza_user","pizzapizza","pizza_db");
+    if(mysqli_connect_errno()){
+        $import_error_message = "Error connecting to the database: " . mysqli_connect_error();
+    }
+    else{
+        try{
+            $contents = file_get_contents(
+                $_FILES['importFile']['tmp_name']);
+            $lines = explode("\n", $contents);
+            for($x = 1; $x < count($lines); $x++){
+                $parsed_csv_line = str_getcsv($lines[$x]);
+                //to-do do something with the parsed data.
+                // ex $parsed_csv_line[0] is the first column
+                //    $parsed_csv_line[1] is the second column
+                // etc...
+                echo implode(" ", $parsed_csv_line);
+            }
+            $import_succeeded = true;
 
-if (mysqli_connect_errno()) {
-    die("Connection failed: " . mysqli_connect_error());
+        }
+        catch(Error $e){
+            $import_error_message = $e->getMessage()
+                ." at:" . $e->getFile()." at line ".$e->getLine();
+        }
+        if($import_attempted == true){
+            if($import_succeeded == true){
+                ?>
+                <h1><span style="color:green;">Import Succeeded</span></h1>
+                <?php
+            }else{
+                ?>
+                <h1>Import Failed</h1>
+                <?php echo $import_error_message; ?>
+                <br/>
+                <?php
+            }
+        }
+    }
 }
-echo "Connected successfully!";
-
-mysqli_close($conn);
 ?>
+<html lang = 'en'>
+<head><title>Pizza Data Import</title></head>
+<body>
+<h1>Pizza data import</h1>
+<form method="post" enctype="multipart/form-data">
+    File: <input type = "file" name = "importFile" />
+    <br/>
+    <input type="submit" value="Upload Data"/>
+</form>
+<?php
+echo "Php is working on playground" ;
+?>
+</body>
+</html>
