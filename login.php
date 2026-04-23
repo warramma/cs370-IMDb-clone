9d666b4aaf8fa80fb4a6c19b866a25a82a19878e
warramma <wrammazy@mail.bradley.edu>
2026-04-23 09:37:26 -0500

    create login page

--- login.php
+++ login.php
@@ -1,19 +1,37 @@
 <?php include_once("components/_header.php"); ?>
 <?php
     //login logic
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    //check for database connection
+    //check for username in the database
+    //grab the password hash from the database
+
+    $hash =  password_hash("test", PASSWORD_BCRYPT);
+    $enteredPassword = 'test';
+    if (password_verify($enteredPassword, $hash)) {
+        echo 'Password is valid!';
+    } else {
+        echo 'Invalid password.';
+    }
+
+}
+
+
 ?>
 <div class="container">
     <div>
         <h1>Login</h1>
-        <div>
-            <label for="username" >Username:</label>
-            <input id="username" type = "text"/>
-            <br/>
-            <label for="password">Password:</label>
-            <input id="password" type="password"/>
-            <br>
-            <a class="btn btn-primary" href="index.php">Log In</a>
-        </div>
+        <form method="post" enctype="multipart/form-data">
+            <div class="input-group md-3">
+                <label class="input-group-text" for="username" >Username:</label>
+                <input id="username" type = "text"/>
+            </div>
+            <div class="input-group md-3">
+                <label class = "input-group-text" for="password">Password:</label>
+                <input id="password" type="password"/>
+            </div>
+            <input class="btn btn-primary" type="submit" value="Log In"/>
+        </form>
     </div>
 
 </div>
