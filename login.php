ebb17d37220beaf4f66bd417f78e54c5d5ec5bc2
JASD <86506578+JASDtubz@users.noreply.github.com>
2026-04-24 13:28:38 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,22 @@
 <?php include_once("components/_header.php"); ?>
 <?php
-    //login logic
+//login logic
 ?>
-<div class="container">
     <div>
-        <h1>Login</h1>
-        <div>
-            <label for="username" >Username:</label>
-            <input id="username" type = "text"/>
-            <br/>
-            <label for="password">Password:</label>
-            <input id="password" type="password"/>
-            <br>
-            <a class="btn btn-primary" href="index.php">Log In</a>
+        <div class="p-5 bg-dark">
+            <h1>Login</h1>
+            <div>
+                <label for="username">Username:</label>
+                <input id="username" type = "text"/>
+                <br/>
+                <label for="password">Password:</label>
+                <input id="password" type="password"/>
+                <br>
+                <a class="btn btn-primary" href="index.php">Log In</a>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 
 
 <?php include_once("components/_footer.php"); ?>
